9d7377d1608895429142c2cc478d4be7a44f71a2
Bradley Meardon <bradley.meardon@gmail.com>
2021-10-11 15:48:46 +0100

    Added new features
    Sets the colour inputs to default on load.
    
    Displays the initial CSS for the gradient on load.
    
    A button added to randomly generate different background colours.

--- script.js
+++ script.js
@@ -2,12 +2,34 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var button = document.querySelector(".random");
 
 function setGradient() {
 	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 	css.textContent = body.style.background + ";";
 }
 
+function setDefaultGradient(){
+	color1.value = "#FF0000";
+	color2.value = "#FFFF00";
+	setGradient();
+}
+
+function randomColour(){
+	var colour = "#" + Math.floor(Math.random()*16777215).toString(16);
+	return colour;
+}
+
+function randomColourSet(){
+	color1.value = randomColour();
+	color2.value = randomColour();
+	setGradient();
+}
+
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+button.addEventListener("click", randomColourSet);
+
+document.onload = setDefaultGradient();
